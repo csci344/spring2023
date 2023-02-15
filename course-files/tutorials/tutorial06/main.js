8844855777f02b590cd056b262278017572abe42
Sarah <vanwars@gmail.com>
2023-02-15 07:24:46 -0500

    Posted Tutorial 6

---
+++ course-files/tutorials/tutorial06/main.js
@@ -0,0 +1,17 @@
+/**
+ * Your logic here (do it however you want).
+ * 
+ * The things you have to have:
+ *    1. A function (i.e., "Event Handler") to initiate the search.
+ *    2. Logic to take the user inputs to build the search query.
+ *    3. Logic to send the search query to the relevant server.
+ *    4. Logic to display the results to the screen.
+ * 
+ * Provider-specific instructions:
+ *    1. If you choose Yelp, allow your user to input both a search term
+ *       and a location.
+ *    2. If you choose Spotify, allow your user to specify both a search term 
+ *       and a resource type (album, artist, or track).
+ *    3. If you choose Twitter, allow your user to specify both a search term
+ *       and a result_type (mixed, recent, or popular).
+ */
