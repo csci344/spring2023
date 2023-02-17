6de5db8e05efe41db8cd33b5ef11a170dcab8231
Sarah <vanwars@gmail.com>
2023-02-17 07:38:09 -0500

    Updates to tutorial 6 starter files

--- course-files/tutorials/tutorial06/main.js
+++ course-files/tutorials/tutorial06/main.js
@@ -10,8 +10,16 @@
  * Provider-specific instructions:
  *    1. If you choose Yelp, allow your user to input both a search term
  *       and a location.
+ *          * See API Tutor for guidance: https://www.apitutor.org/
+ *          * Sample query: https://www.apitutor.org/yelp/simple/v3/businesses/search?location=Asheville,%20NC&term=pizza
+ * 
  *    2. If you choose Spotify, allow your user to specify both a search term 
  *       and a resource type (album, artist, or track).
+ *          * See API Tutor for guidance: https://www.apitutor.org/
+ *          * Sample query: https://www.apitutor.org/spotify/simple/v1/search?q=beyonce&type=track
+ * 
  *    3. If you choose Twitter, allow your user to specify both a search term
  *       and a result_type (mixed, recent, or popular).
+ *          * See API Tutor for guidance: https://www.apitutor.org/
+ *          * Sample query: https://www.apitutor.org/twitter/simple/1.1/search/tweets.json?q=cats&result_type=popular
  */
