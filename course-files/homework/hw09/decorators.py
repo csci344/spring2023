ffed671a6728e77fb79d3188146addd6db906451
Sarah <vanwars@gmail.com>
2023-04-24 11:19:50 -0400

    minor updates to HW9

--- course-files/homework/hw09/decorators.py
+++ course-files/homework/hw09/decorators.py
@@ -1,12 +1,16 @@
 import flask_jwt_extended
 from flask import redirect
 from flask_jwt_extended.exceptions import NoAuthorizationError
+from jwt import ExpiredSignatureError
 
 def jwt_or_login(view_function):
     def wrapper(*args, **kwargs):
         try:
-            flask_jwt_extended.verify_jwt_in_request()
+            jwt_header, jwt_data = flask_jwt_extended.verify_jwt_in_request()
+            print(jwt_header, jwt_data)
             return view_function(*args, **kwargs)
+        except ExpiredSignatureError:
+            return redirect('/login', code=302)
         except NoAuthorizationError:
             return redirect('/login', code=302)
             
